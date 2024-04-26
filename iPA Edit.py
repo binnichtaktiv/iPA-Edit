ff01681e40bc015bf1781bcd67c125a8153531ed
binnichtaktiv <96953964+binnichtaktiv@users.noreply.github.com>
2024-04-26 17:37:43 +0200

    progress

--- iPA Edit.py
+++ iPA Edit.py
@@ -23,12 +23,18 @@ parser.add_argument("-v", metavar="app version", type=str,
                     help="change app version")
 parser.add_argument("-p", metavar="app icon", type=str,
                     help="change app icon")
-parser.add_argument("-k",action="store_true",
+parser.add_argument("-f",action="store_true",
+                    help="enable document browser")
+parser.add_argument("-d", action="store_true",
+                    help="export .dylib(s) that are injected in that iPA")
+parser.add_argument("-s", action="store_true",
+                    help="sign iPA(s) with a certificate")
+parser.add_argument("-k", action="store_true",
                     help="keep source iPA")
 
 args = parser.parse_args()
-
-if os.path.exists(args.o):
+    
+if os.path.exists(args.o) and not args.d and not args.s:
     overwrite = input(f"[<] {args.o} already exists. overwrite? [Y/n] ").lower().strip()
     if overwrite in ("y", "yes", ""):
         del overwrite
@@ -63,11 +69,12 @@ def unzip_ipa(ipa_path):
     app_path = os.path.join(payload_path, app_folder)
     return app_path, zip_path, payload_path
 
-def zip_ipa(ipa_path, app_path, payload_path):
+def zip_ipa(ipa_path, payload_path):
+    
     print("[*] generating iPA...")
     if args.o.endswith('.ipa'):
         args.o = args.o[:-4]
-    shutil.make_archive(args.o, 'zip', payload_path)
+    shutil.make_archive(args.o, 'zip', os.path.dirname(payload_path), os.path.basename(payload_path))
 
     args.o = args.o + '.zip'
     output_name = args.o.replace('.zip', '.ipa')
@@ -80,11 +87,11 @@ def zip_ipa(ipa_path, app_path, payload_path):
     shutil.rmtree(payload_path)
 
 
-if args.i:
+if not args.s:
     ipa_path = args.i
     app_path, zip_path, payload_path = unzip_ipa(ipa_path)
 
-if args.n or args.b or args.v or args.k:
+if args.n or args.b or args.v or args.k or args.f:
 
     plist_path = os.path.join(app_path, 'Info.plist')
     with open(plist_path, 'rb') as plist:
@@ -126,28 +133,114 @@ if args.n or args.b or args.v or args.k:
                 img.save(args.p, "PNG")
 
         with Image.open(args.p) as img:
-            img.resize((120, 120)).save(os.path.join(app_path, "changedicon@2x.png"), "PNG")
-            img.resize((152, 152)).save(os.path.join(app_path, f"changedicon@2x~ipad.png"), "PNG")
+            img.resize((120, 120)).save(os.path.join(app_path, "changedicon_60x60@2x.png"), "PNG")
+            img.resize((152, 152)).save(os.path.join(app_path, f"changedicon_76x76@2x~ipad.png"), "PNG")
         
         pl_content["CFBundleIcons"] = {
             "CFBundlePrimaryIcon": {
-                "CFBundleIconFiles": [f"{icon_60x60}@2x"],
+                "CFBundleIconFiles": [f"{icon_60x60}"],
                 "CFBundleIconName": icon
             }
         }
 
         pl_content["CFBundleIcons~ipad"] = {
             "CFBundlePrimaryIcon": {
-                "CFBundleIconFiles": [f"{icon_60x60}@2x", f"{icon_76x76}@2x~ipad"],
+                "CFBundleIconFiles": [f"{icon_60x60}", f"{icon_76x76}"],
                 "CFBundleIconName": icon
             }
         }
-
+        print("[*] changed ap icon")
+    
+    if args.f:
+        pl_content['LSSupportsOpeningDocumentsInPlace'] = True
+        pl_content['UIFileSharingEnabled'] = True
+        print("[*] enabled document browser")
 
     with open(plist_path, 'wb') as plist:
         plistlib.dump(pl_content, plist)
 
+if args.d:
+    dylib_files = []
+    for root, _, files in os.walk(app_path):
+        for file in files:
+            if file.endswith('.dylib'):
+                dylib_files.append(os.path.join(root, file))
+    if not dylib_files:
+        print("[!] no dylibs found")
+        
+    for index, file in enumerate(dylib_files, start=1):
+        print(f'{index}: {os.path.basename(file)}')
+
+    selected_files = input("[?] enter the numbers of the files to be exported separated by commas\nenter 'exit' to exit without exporting\n")
+
+    if selected_files.strip().lower() == 'exit':
+        os.rename(zip_path, ipa_path)
+        shutil.rmtree(payload_path)
+        sys.exit("[!] you decided to exit!\n[*] cleaned up temp files")
+
+    selected_nums = [int(num.strip()) - 1 for num in selected_files.split(',')]
+    selected_dylibs = [dylib_files[index] for index in selected_nums]
+    if not os.path.exists(args.o):
+        os.rename(zip_path, ipa_path)
+        shutil.rmtree(payload_path)
+        sys.exit("[!] please check your output folder! it doesn't seem to exist\n[*] cleaned up temp files")
+
+    for file in selected_dylibs:
+        shutil.copy(file, args.o)
+    print("[*] exported .dylib(s) successfully")
+    
+    
+if args.s:
+    p12_path = ""
+    mb_path = ""
+    try:
+        subprocess.run(["zsign"], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        zsign_path = 'zsign'
+    except FileNotFoundError:
+        zsign_path = input("[?] enter zsign excutable path:\n")
+
+    if not args.i.endswith(".ipa"):
+        output_dir = os.path.join(args.o, 'signed_iPAs')
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        dir_files = os.listdir(args.i)
+        ipa_files = [i for i in dir_files if i.endswith('.ipa')]
+                
+        for file in os.listdir(args.i):
+            if file.endswith(".p12"):
+                p12_path = os.path.join(args.i, file)
+            elif file.endswith(".mobileprovision"):
+                mb_path = os.path.join(args.i, file)
+
+    if p12_path and mb_path:
+        use_cert = input(f"[?] .p12 and .mobileprovision file was found at: {args.i}. do you want to use this certificate? [Y/n] ").lower().strip()
+        if use_cert in ("y", "yes", ""):
+            print(f"[*] {mb_path} and {p12_path} will be used")
+    else:
+        p12_path = input("[?] enter .p12 file path:\n")
+        mb_path = input("[?] enter .mobileprovision file path:\n")
+        
+    cert_pw = input("[?] enter certificate password:\n")
+     
+    if not args.i.endswith(".ipa"):       
+        for ipa_file in ipa_files:
+                ipa_path = os.path.join(args.i, ipa_file)
+                ipa_name = os.path.basename(ipa_path)
+                signed_ipa = output_dir + "/" + ipa_name
+                cmd = f'"{zsign_path}" -k "{p12_path}" -m "{mb_path}" -p "{cert_pw}" -o "{signed_ipa}" -z 9 "{ipa_path}"'
+                subprocess.run(cmd, shell=True)
+    else:
+        if not args.o.endswith(".ipa"):
+            args.o += '.ipa'
+        cmd = f'"{zsign_path}" -k "{p12_path}" -m "{mb_path}" -p "{cert_pw}" -o "{args.o}" -z 9 "{args.i}"'
+        subprocess.run(cmd, shell=True)
 
 
-zip_ipa(ipa_path, app_path, payload_path)
+if not args.d and not args.s:
+    zip_ipa(ipa_path, payload_path)
+elif not args.s:
+    os.rename(zip_path, ipa_path)
+    shutil.rmtree(payload_path)
+    print("[*] deleted temp files")
 
+    
